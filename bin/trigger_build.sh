8c2601a9010377ec326d8139c820a4041fa26db3
Szilveszter Balogh <szilveszterbalogh@invenshure.com>
2018-04-05 18:06:47 +0200

    For testing use the szb/ci_trigger_pr branch

--- bin/trigger_build.sh
+++ bin/trigger_build.sh
@@ -1,4 +1,4 @@
 TRAVIS_TOKEN=$1
 MESSAGE="Build triggered by repo:core branch:$2"
-POST_DATA="{\"request\": {\"branch\":\"master\", \"message\":\"$MESSAGE\"}}"
+POST_DATA="{\"request\": {\"branch\":\"szb/ci_trigger_pr\", \"message\":\"$MESSAGE\"}}"
 curl -X POST -H "Content-Type: application/json" -H "Accept: application/json" -H "Travis-API-Version: 3" -H "User-Agent: Flywheel ci" -H "Authorization: token $TRAVIS_TOKEN" -d "$POST_DATA" https://api.travis-ci.com/repo/flywheel-io%2Finstaller/requests
