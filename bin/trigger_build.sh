744c0f7a7cb542a96e5709822811bb7c86643e63
Szilveszter Balogh <szilveszterbalogh@invenshure.com>
2018-04-05 17:38:12 +0200

    Pass in travis token as a parameter

--- bin/trigger_build.sh
+++ bin/trigger_build.sh
@@ -1,1 +1,1 @@
-curl -X POST -H "Content-Type: application/json" -H "Accept: application/json" -H "Travis-API-Version: 3" -H "User-Agent: Flywheel ci" -H "Authorization: token $(travis token --no-interactive --pro)" -d '{"request": {"branch":"master"}}' https://api.travis-ci.com/repo/flywheel-io%2Finstaller/requests
+curl -X POST -H "Content-Type: application/json" -H "Accept: application/json" -H "Travis-API-Version: 3" -H "User-Agent: Flywheel ci" -H "Authorization: token $1" -d '{"request": {"branch":"master"}}' https://api.travis-ci.com/repo/flywheel-io%2Finstaller/requests
