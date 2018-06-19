efaae371808f4dc16d22cd285d1232b744d5cbf2
Szilveszter Balogh <szilveszterbalogh@invenshure.com>
2018-06-19 14:17:47 +0200

    Update build trigger script to send the ORIGIN_GIT_REF and ORIGIN_IMAGE_REF as well

--- bin/trigger_build.sh
+++ bin/trigger_build.sh
@@ -1,7 +1,16 @@
 TRAVIS_TOKEN=$1
-TRIGGER_IMAGE=$2
+ORIGIN_IMAGE=$2
 TARGET_REPO=$3
 TARGET_BRANCH=$4
-MESSAGE="Build triggered by image:$TRIGGER_IMAGE"
-POST_DATA="{\"request\": {\"branch\":\"$TARGET_BRANCH\", \"message\":\"$MESSAGE\", \"config\": {\"env\": {\"CORE_IMAGE\": \"$TRIGGER_IMAGE\"}}}}"
+ORIGIN_BRANCH=$5
+ORIGIN_TAG=$6
+if [[ -n $ORIGIN_TAG ]]; then
+	ORIGIN_GIT_REF=$ORIGIN_TAG
+	ORIGIN_IMAGE_REF=$ORIGIN_TAG
+else
+	ORIGIN_GIT_REF=$ORIGIN_BRANCH
+	ORIGIN_IMAGE_REF="$ORIGIN_BRANCH.latest"
+fi
+MESSAGE="Build triggered by image:$ORIGIN_IMAGE"
+POST_DATA="{\"request\": {\"branch\":\"$TARGET_BRANCH\", \"message\":\"$MESSAGE\", \"config\": {\"env\": {\"CORE_IMAGE\": \"$ORIGIN_IMAGE\"}, {\"ORIGIN_GIT_REF\": \"$ORIGIN_GIT_REF\"}, {\"ORIGIN_IMAGE_REF\": \"$ORIGIN_IMAGE_REF\"}}}}"
 curl -X POST -H "Content-Type: application/json" -H "Accept: application/json" -H "Travis-API-Version: 3" -H "User-Agent: Flywheel ci" -H "Authorization: token $TRAVIS_TOKEN" -d "$POST_DATA" https://api.travis-ci.com/repo/$TARGET_REPO/requests
