1c7693eeeebec68e8771757b5fd1b703667560dd
Szilveszter Balogh <szilveszterbalogh@invenshure.com>
2018-06-20 10:49:52 +0200

    Add ORIGIN_TAG parameter and trigger build with custom branch name

--- bin/trigger_build.sh
+++ bin/trigger_build.sh
@@ -5,12 +5,14 @@ TARGET_BRANCH=$4
 ORIGIN_BRANCH=$5
 ORIGIN_TAG=$6
 if [[ -n $ORIGIN_TAG ]]; then
-	ORIGIN_GIT_REF=$ORIGIN_TAG
-	ORIGIN_IMAGE_REF=$ORIGIN_TAG
+    ORIGIN_TAG=$ORIGIN_TAG
+    ORIGIN_GIT_REF=$ORIGIN_TAG
+    ORIGIN_IMAGE_REF=$ORIGIN_TAG
 else
-	ORIGIN_GIT_REF=$ORIGIN_BRANCH
-	ORIGIN_IMAGE_REF="$ORIGIN_BRANCH.latest"
+    ORIGIN_TAG=""
+    ORIGIN_GIT_REF="origin/$ORIGIN_BRANCH"
+    ORIGIN_IMAGE_REF="$ORIGIN_BRANCH.latest"
 fi
 MESSAGE="Build triggered by image:$ORIGIN_IMAGE"
-POST_DATA="{\"request\": {\"branch\":\"$TARGET_BRANCH\", \"message\":\"$MESSAGE\", \"config\": {\"env\": {\"CORE_IMAGE\": \"$ORIGIN_IMAGE\", \"ORIGIN_GIT_REF\": \"$ORIGIN_GIT_REF\", \"ORIGIN_IMAGE_REF\": \"$ORIGIN_IMAGE_REF\"}}}}"
+POST_DATA="{\"request\": {\"branch\":\"$TARGET_BRANCH\", \"message\":\"$MESSAGE\", \"config\": {\"env\": {\"CORE_IMAGE\": \"$ORIGIN_IMAGE\", \"ORIGIN_GIT_REF\": \"$ORIGIN_GIT_REF\", \"ORIGIN_IMAGE_REF\": \"$ORIGIN_IMAGE_REF\", \"ORIGIN_TAG\": \"$ORIGIN_TAG\"}}}}"
 curl -X POST -H "Content-Type: application/json" -H "Accept: application/json" -H "Travis-API-Version: 3" -H "User-Agent: Flywheel ci" -H "Authorization: token $TRAVIS_TOKEN" -d "$POST_DATA" https://api.travis-ci.com/repo/$TARGET_REPO/requests
