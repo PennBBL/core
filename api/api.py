4a1f3a7f0de74753cf35d745cba53ebf370d469b
David Farkas <david.farkas93@gmail.com>
2018-12-19 17:57:49 +0100

    Remove gcp endpoints

--- api/api.py
+++ api/api.py
@@ -8,7 +8,6 @@ from .handlers.confighandler            import Config, Version
 from .handlers.containerhandler         import ContainerHandler
 from .handlers.dataexplorerhandler      import DataExplorerHandler, QueryHandler
 from .handlers.devicehandler            import DeviceHandler
-from .handlers.gcphandler               import GCPHandler, GHCHandler, BigQueryHandler
 from .handlers.grouphandler             import GroupHandler
 from .handlers.listhandler              import FileListHandler, NotesListHandler, PermissionsListHandler, TagsListHandler
 from .handlers.modalityhandler          import ModalityHandler
@@ -375,27 +374,5 @@ endpoints = [
 
         route('/<par_cont_name:groups>/<par_id:{gid}>/<cont_name:projects>', ContainerHandler, h='get_all', m=['GET']),
         route('/<par_cont_name:{cname}>/<par_id:{oid}>/<cont_name:{cname}>', ContainerHandler, h='get_all', m=['GET']),
-
-        # Google Cloud Platform
-        prefix('/gcp', [
-            route('/token',        GCPHandler, h='generate_token',     m=['GET']),
-            route('/auth',         GCPHandler, h='get_auth_url',       m=['GET']),
-            route('/auth/token',   GCPHandler, h='get_oauth2_token',   m=['GET', 'POST']),
-            route('/auth/revoke',  GCPHandler, h='revoke_token',       m=['POST']),
-            route('/config',       GCPHandler, h='get_default_config', m=['GET']),
-            # Google Healthcare
-            prefix('/hc', [
-                route('/query',     GHCHandler, h='run_query',          m=['POST']),
-                route('/details',   GHCHandler, h='run_details_query',  m=['POST']),
-                route('/import',    GHCHandler, h='run_import',         m=['POST']),
-                route('/jobs',      GHCHandler, h='get_jobs',           m=['GET']),
-                route('/statistic', GHCHandler, h='run_statistics',     m=['POST']),
-                route('/schema',    GHCHandler, h='get_schema',         m=['POST']),
-            ]),
-            # Google BigQuery
-            prefix('/bq', [
-                route('/export',    BigQueryHandler,    h='export_view',         m=['POST']),
-            ]),
-        ]),
     ]),
 ]
